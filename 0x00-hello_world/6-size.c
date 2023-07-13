a2c2157e424b523b360e66d36214e3d52fc5bda1
imoteriannah <imoter@gmail.com>
2023-07-13 16:21:58 -0700

    using sizeof operator

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-	printf("%lu\n", sizeof(char));
-	printf("%lu\n", sizeof(int));
-	printf("%lu", sizeof(long));
-	printf("%lu", sizeof(long long));
-	printf("%lu", sizeof(float));
+	printf("%lu Size of char:\n", sizeof(char));
+	printf("%lu Size of int:\n", sizeof(int));
+	printf("%lu Size of long:\n", sizeof(long));
+	printf("%lu Size of long long:\n", sizeof(long long));
+	printf("%lu Size of float:\n", sizeof(float));
 	return (0);
 }
