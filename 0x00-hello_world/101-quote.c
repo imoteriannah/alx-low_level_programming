27ffde96ca65dd979d4ee8825716c509d66de9bd
imoteriannah <imoter@gmail.com>
2023-07-14 10:15:01 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-*main - Program entry point
-*
-*Return: Always 1 (success)
-*/
+ * main - Entry point
+ * Return:1
+ */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
