d4e46de48d1a2b47ce622e8ba34c1cd897d85640
imoteriannah <imoter@gmail.com>
2023-07-14 21:03:58 -0700

    positive is better

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Porogram enttry
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int last_D;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	last_D = n % 10;
+
+	if (last_D > 5)
+		printf("last digit of %i is %i and is greater than 5\n", n, last_D);
+	else if (last_D == 0)
+		printf("last digit of %i is %i and is 0\n", n, last_D);
+	else if (last_D < 6)
+		printf("last digit of %i is %i and is less than 6 and not 0\n", n, last_D);
+	return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Hello C variables and if else
