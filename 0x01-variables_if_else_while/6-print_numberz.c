6700f600fe7d24c1782ae02498043d4b3e0f622b
imoteriannah <imoter@gmail.com>
2023-07-14 21:59:57 -0700

    nnumber

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: prints all single digit numbers of base 10 system
+ * Return: 0
+ */
+int main(void)
+{
+	int  b = 0;
+
+	do {
+		putchar(b);
+		b++;
+	} while (b <= 9);
+	putchar('\n');
+	return (0);
+}
