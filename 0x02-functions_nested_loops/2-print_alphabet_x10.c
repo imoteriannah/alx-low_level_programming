1a833863d181488833a7e344e75ee736abca56bf
imoteriannah <imoter@gmail.com>
2023-07-18 16:38:26 -0700

    10xalphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		char c = 'a';
+
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+	}
+}
