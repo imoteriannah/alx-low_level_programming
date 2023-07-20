ea9cdbc4ce1d1f5ace222979dece72c30831a03a
imoteriannah <imoter@gmail.com>
2023-07-20 03:55:16 -0700

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Function to test functionality of positive_or_negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c = 0;
+
+	positive_or_negative(c);
+	return (0);
+}
