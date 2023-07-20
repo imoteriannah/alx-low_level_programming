1cdab71bfd3d186bc785e05458206f30f231b76a
imoteriannah <imoter@gmail.com>
2023-07-20 06:17:30 -0700

    numbers print

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#inlcude "main.h"
+
+/**
+*print_numbers - function to print numbers from 0 to 9
+*return: 0
+*/
+
+void print_numbers(void)
+{
+	char i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar('0' + i)
+			i++
+	}
+	_putchar('\n');
+}
