05d4121da45cec0252a16e4f07bce5f3fd5fdede
imoteriannah <imoter@gmail.com>
2023-07-20 06:08:04 -0700

    multiply function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - checks if the character is Uppercase
+ * @a:  argument to multiply
+ * @b: int argument to multiply
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
