c64e63499f1db64234fcc85c6c01c616be5b991e
imoteriannah <imoter@gmail.com>
2023-07-20 04:49:49 -0700

    main 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,7 +11,7 @@ int i;
 
 printf("Infinite loop incoming :(\n");
 
-	i = 0;
+i = 0;
 /*
 *while (i < 10)
 *{
@@ -19,5 +19,6 @@ printf("Infinite loop incoming :(\n");
 *}
 */
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
