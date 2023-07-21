e89915b54420168688c4062718e28289874d5edf
imoteriannah <imoter@gmail.com>
2023-07-20 18:10:54 -0700

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check for a digit
+ * @n : number of \\ to print
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+
+	int i = 0, c;
+
+	while (i < n && n > 0)
+	{
+		c = 0;
+		while (c < i)
+		{
+			_putchar(' ');
+			ii++;
+		}
+
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+
+}
