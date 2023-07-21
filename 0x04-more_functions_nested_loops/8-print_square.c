b1791ac0f52914053ca571a2b0ebe2be00ef374a
imoteriannah <imoter@gmail.com>
2023-07-20 18:14:08 -0700

    print sqaure

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - check for a digit
+ * @n : number of _ to print
+ * Return: 0
+ */
+
+void print_square(int n)
+{
+
+	int i = 0, c;
+
+	while (i < n && n > 0)
+	{
+		c = 0;
+		while (c < n)
+		{
+			_putchar('#');
+			ii++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		_putchar('\n');
+
+}
