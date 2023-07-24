4c8da3e0b0e7141071673f8ac1c361e16d3ed17a
imoteriannah <imoter@gmail.com>
2023-07-24 14:55:31 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function to rset pointer to 98
+ * @n: pointer to whose value is to be reset.
+ * return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
