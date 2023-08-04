8156ad33c57f170d500fcd0797e87eab348013a7
imoteriannah <imoter@gmail.com>
2023-08-04 14:02:40 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stido.h>
+
+/**
+ * main - Program entry point
+ * @argc: number of arguments passed to the program
+ * @argv: array of the arguments passed
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
