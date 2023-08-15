051a8d5ef36780d5a3ba7259e7b1a12ac05012f4
imoteriannah <imoter@gmail.com>
2023-08-14 21:26:11 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Main program entry point
+ *function to print the name of file compiled from
+ * Return: 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
